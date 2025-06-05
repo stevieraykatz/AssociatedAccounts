92671fce3187ff58ae1376d01d35c5b4c3897711
katzman <steve.katzman@coinbase.com>
2025-06-05 10:50:35 -0700

    Get PoC working

--- src/AssociatedAccountsLib.sol
+++ src/AssociatedAccountsLib.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.23;
 
 import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import {console} from "forge-std/console.sol";
 
-/// @notice Helper Lib for creating and validating AssociatedAccount records and the resulting
+
+/// @notice Helper Lib for creating, signing and validating AssociatedAccount records and the resulting
 ///     SignedAssociationRecords.
 library AssociatedAccountsLib {
-    /// @dev Precomputed `typeHash` used to produce EIP-712 compliant hash when applying the anti
-    ///      cross-account-replay layer.
-    ///
-    ///      The original hash must either be:
-    ///         - An EIP-191 hash: keccak256("\x19Ethereum Signed Message:\n" || len(someMessage) || someMessage)
+
+    event AssociationCreated();
+
+    /// @dev Precomputed `typeHash` used to produce EIP-712 compliant hash.
+    ///      The original hash must be:
     ///         - An EIP-712 hash: keccak256("\x19\x01" || someDomainSeparator || hashStruct(someStruct))
     bytes32 private constant _MESSAGE_TYPEHASH = keccak256("AssociatedAccountRecord(address account, bytes data)");
 
@@ -22,7 +24,7 @@ library AssociatedAccountsLib {
         bytes data;
     }
 
-    /// @notice Helper struct for pairing signatures with the associated signer.
+    /// @notice Helper struct for sharing signed associations.
     struct SignedAssociationRecord {
         /// @dev The address of the account that signed the payload.
         address signer;
@@ -30,6 +32,17 @@ library AssociatedAccountsLib {
         AssociatedAccountRecord record;
         /// @dev The signature data.
         bytes signature;
+        /// @dev Association state showing whether this is an initiation or approval
+    }
+
+    enum AssociationState {
+        INITIATED,
+        APPROVED
+    }
+
+    /// @notice Helper for validating the contents of a SignedAssociationRecord.
+    function validateAssociatedAccount(SignedAssociationRecord memory sar) external view returns (bool) {
+        return _isValidSignature(eip712Hash(sar.record), sar);
     }
 
     /// @notice Validates the `signature` against the given `hash`.
@@ -39,30 +52,15 @@ library AssociatedAccountsLib {
     ///      cross-account-replay layer on the given `hash` (i.e., verification is run on the replay-safe
     ///      hash version).
     ///
-    /// @param signature The signature of the replay-safe hash to validate.
-    ///
     /// @return result `0x1626ba7e` if validation succeeded, else `0xffffffff`.
-    function isValidSignature(address account, bytes memory data, bytes calldata signature) public view returns (bytes4 result) {
-        if (_isValidSignature({hash: replaySafeHash(account, data), signature: signature})) {
+    function isValidSignature(bytes32 hash, bytes memory signature) public view returns (bytes4 result) {
+        SignedAssociationRecord memory sar = abi.decode(signature, (SignedAssociationRecord));
+        if (_isValidSignature(hash, sar)) {
             // bytes4(keccak256("isValidSignature(bytes32,bytes)"))
             return 0x1626ba7e;
         }
 
         return 0xffffffff;
-    }
-
-    /// @notice Wrapper around `_eip712Hash()` to produce a replay-safe hash fron the given `hash`.
-    ///
-    /// @dev The returned EIP-712 compliant replay-safe hash is the result of:
-    ///      keccak256(
-    ///         \x19\x01 ||
-    ///         this.domainSeparator ||
-    ///         hashStruct(AssociatedAccountRecord({ account: `account`, data: `data`}))
-    ///      )
-    ///
-    /// @return The corresponding replay-safe hash.
-    function replaySafeHash(address account, bytes memory data) public view returns (bytes32) {
-        return _eip712Hash(account, data);
     }
 
     /// @notice Returns the `domainSeparator` used to create EIP-712 compliant hashes.
@@ -71,31 +69,20 @@ library AssociatedAccountsLib {
     ///      See https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator.
     ///
     /// @return The 32 bytes domain separator result.
-    function domainSeparator() public view returns (bytes32) {
+    function domainSeparator() public pure returns (bytes32) {
         (string memory name, string memory version) = _domainNameAndVersion();
         return keccak256(
             abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256("EIP712Domain(string name,string version)"),
                 keccak256(bytes(name)),
-                keccak256(bytes(version)),
-                block.chainid,
-                address(this)
+                keccak256(bytes(version))
             )
         );
     }
 
-    /// @notice Helper for validating the contents of a SignedAssociationRecord.
-    function validateAssociatedAccount(SignedAssociationRecord calldata signedAssociation)
-        public
-        view
-        returns (bytes4)
-    {
-        return isValidSignature(signedAssociation.record.account, signedAssociation.record.data, signedAssociation.signature);
-    }
-
     /// @notice Helper for fetching the EIP-712 signature hash for a provided AssociatedAccountRecord.  
-    function eip712Hash(AssociatedAccountRecord calldata record) external view returns (bytes32) {
-        return _eip712Hash(record.account, record.data);
+    function eip712Hash(AssociatedAccountRecord memory aar) public pure returns (bytes32) {
+        return _eip712Hash(aar.account, aar.data);
     }
 
     /// @notice Returns the domain name and version to use when creating EIP-712 signatures.
@@ -105,20 +92,16 @@ library AssociatedAccountsLib {
     /// @return name    The user readable name of signing domain.
     /// @return version The current major version of the signing domain.
     function _domainNameAndVersion() internal pure returns (string memory name, string memory version) {
-        return ("AsoociatedAccount", "1");
+        return ("AssociatedAccount", "1");
     }
 
     /// @notice Validates the `signature` against the given `hash`.
     ///
     /// @dev MUST be defined by the implementation.
     ///
-    /// @param hash      The hash whose signature has been performed on.
-    /// @param signature The signature associated with `hash`.
-    ///
     /// @return `true` is the signature is valid, else `false`.
-    function _isValidSignature(bytes32 hash, bytes calldata signature) internal view returns (bool) {
-        SignedAssociationRecord memory signedAssociation = abi.decode(signature, (SignedAssociationRecord));
-        return SignatureChecker.isValidSignatureNow(signedAssociation.signer, hash, signedAssociation.signature);
+    function _isValidSignature(bytes32 hash, SignedAssociationRecord memory sar) internal view returns (bool) {
+        return SignatureChecker.isValidSignatureNow(sar.signer, hash, sar.signature);
     }
 
     /// @notice Returns the EIP-712 typed hash of the `AssociatedAccountRecord(address account, bytes data)` data structure.
@@ -128,7 +111,7 @@ library AssociatedAccountsLib {
     /// @dev See https://eips.ethereum.org/EIPS/eip-712#specification.
     ////
     /// @return The resulting EIP-712 hash.
-    function _eip712Hash(address account, bytes memory data) internal view returns (bytes32) {
+    function _eip712Hash(address account, bytes memory data) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked("\x19\x01", domainSeparator(), _hashStruct(account, data)));
     }
 
@@ -139,8 +122,8 @@ library AssociatedAccountsLib {
     /// @dev See https://eips.ethereum.org/EIPS/eip-712#definition-of-hashstruct.
     ///
     /// @return The EIP-712 `hashStruct` result.
-    function _hashStruct(address account, bytes memory data) internal view returns (bytes32) {
-        return keccak256(abi.encode(_MESSAGE_TYPEHASH, account, data));
+    function _hashStruct(address account, bytes memory data) internal pure returns (bytes32) {
+        return keccak256(abi.encode(_MESSAGE_TYPEHASH, account, keccak256(data)));
     }
 
 }
--- test/AssociatedAccountsBase.t.sol
+++ test/AssociatedAccountsBase.t.sol
@@ -3,37 +3,66 @@ pragma solidity ^0.8.23;
 
 import {Test} from "forge-std/Test.sol";
 
+import {console} from "forge-std/console.sol";
+
 import {AssociatedAccountsLib} from "src/AssociatedAccountsLib.sol";
 
 contract AssociatedAccountsBase is Test {
+    using AssociatedAccountsLib for *;
 
-    uint256 pkeyA = 0x0A;
-    address A = vm.addr(pkeyA);
-    uint256 pkeyB = 0x0B;
-    address B = vm.addr(pkeyB);
+    uint256 pkeyApprover = 0x0A;
+    address approver = vm.addr(pkeyApprover);
+    uint256 pkeyInitiator = 0x0B;
+    address initiator = vm.addr(pkeyInitiator);
 
     bytes4 VALID_SIGNATURE = 0x1626ba7e;
     bytes4 INVALID_SIGNATURE = 0xffffffff;
     
     function setUp() public {}
 
-    function test_AllowsAnAccountToCreateAnSAR() public view {
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pkeyB, AssociatedAccountsLib.eip712Hash(_getDefaultRecord()));
-        bytes memory signatureData = abi.encodePacked(v, r, s);
-
-        AssociatedAccountsLib.SignedAssociationRecord memory sar = AssociatedAccountsLib.SignedAssociationRecord({
-            signer: B, 
-            record: _getDefaultRecord(),
-            signature: signatureData
-        });
-
-        assertEq(AssociatedAccountsLib.validateAssociatedAccount(sar), VALID_SIGNATURE);
-    }
-
-    function _getDefaultRecord() internal view returns (AssociatedAccountsLib.AssociatedAccountRecord memory) {
-        return AssociatedAccountsLib.AssociatedAccountRecord({
-            account: A,
+    function test_AllowsTwoAccountsToAssociate() public view {
+        /// STEP 1: the Initiator builds and signs an AAR, associating the initiator and approver addresses
+        
+        // 1.A/B: Build the AAR 
+        AssociatedAccountsLib.AssociatedAccountRecord memory step1_aar = AssociatedAccountsLib.AssociatedAccountRecord({
+            account: approver,
             data: ""
         });
+
+        // 1.C: Sign the AAR
+        bytes32 step1_hash = AssociatedAccountsLib.eip712Hash(step1_aar);
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(pkeyInitiator, step1_hash);
+
+        // 1.D: Return an SAR
+        bytes memory step1_signatureData = abi.encodePacked(r1, s1, v1);
+        AssociatedAccountsLib.SignedAssociationRecord memory step1_sar = AssociatedAccountsLib.SignedAssociationRecord({
+            signer: initiator, 
+            record: step1_aar,
+            signature: step1_signatureData
+        });
+
+        assertTrue(step1_sar.validateAssociatedAccount());
+
+        /// STEP 2: The Approver builds and signs an AAR, accepting the signed payload from Step 1 as `data`.
+        // 2.A: Build the AAR including the SAR from step 1 as the data payload. 
+        AssociatedAccountsLib.AssociatedAccountRecord memory step2_aar = AssociatedAccountsLib.AssociatedAccountRecord({
+            account: initiator,
+            data: abi.encode(step1_sar)
+        });
+
+        // 2.B: Sign the AAR
+        bytes32 step2_hash = AssociatedAccountsLib.eip712Hash(step2_aar);
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(pkeyApprover, step2_hash);
+
+        // 2.C: Return the final SAR
+        bytes memory step2_signatureData = abi.encodePacked(r2, s2, v2);
+        AssociatedAccountsLib.SignedAssociationRecord memory step2_sar = AssociatedAccountsLib.SignedAssociationRecord({
+            signer: approver,
+            record: step2_aar,
+            signature: step2_signatureData
+        });
+
+        assertTrue(step2_sar.validateAssociatedAccount());
     }
+
 }
