70f59206167d4ddb9dabf9a46e7b45c303746dd6
katzman <steve.katzman@coinbase.com>
2025-10-29 14:20:45 -0700

    Adjust interface to reflect latest spec

--- src/AssociatedAccounts.sol
+++ src/AssociatedAccounts.sol
@@ -5,45 +5,49 @@ pragma solidity ^0.8.24;
 interface AssociatedAccounts {
     /// @notice Represents an association between two accounts.
     struct AssociatedAccountRecord {
-        /// @dev The CAIP-10 address of the initiating account.
-        string initiator;
-        /// @dev The CAIP-10 address of the approving account
-        string approver;
-        /// @dev Optional 4-byte selector for interfacing with the `data` field.
+        /// @dev The ERC-7930 binary representation of the iniating account's address.
+        bytes initiator;
+            /// @dev The ERC-7930 binary representation of the approving account's address.
+        bytes approver;
+            /// @dev Optional 4-byte selector for interfacing with the `data` field.
         bytes4 interfaceId;
         /// @dev Optional additional data.
         bytes data;
     }
 
-    /// @notice Helper struct for sharing a signed association.
+    /// @notice Complete payload containing a finalized association.
     struct SignedAssociationRecord {
-        /// @dev The timestamp the association was originated.
-        uint128 originatedAt;
-        /// @dev The timestamp the association was revoked, `0` if active.
+        /// @dev The timestamp from which the association is valid.
+        uint128 validAt;
+        /// @dev The timestamp the association was revoked.
         uint128 revokedAt;
         /// @dev The signature of the initiator.
         bytes initiatorSignature;
+        /// @dev The initiator curve specifier.
+        Curve initiatorCurve;
         /// @dev The signature of the approver.
         bytes approverSignature;
-        /// @dev The signed AssociatedAccountRecord.
+        /// @dev The approver curve specifier.
+        Curve approverCurve;
+        /// @dev The underlying AssociatedAccountRecord.
         AssociatedAccountRecord record;
     }
 
     /// @notice Emitted when a SignedAssociationRecord completes its approval process.
     ///
-    /// @param initiator The indexed CAIP10 address of the account that initiated the association.
-    /// @param approver The indexed CAIP10 address of the account that accepted and completed the association.
     /// @param uuid The indexed uuid for the SignedAssociationRecord.
+    /// @param initiator The ERC-7930 address of the account that initiated the association.
+    /// @param approver The ERC-7930 address of the account that accepted and completed the association.
     /// @param sar The completed SignedAssociationRecord for the association between `initiator` and `approver`.
     event AssociationCreated(
-        string indexed initiator, string indexed approver, bytes32 indexed uuid, SignedAssociationRecord sar
+        bytes32 indexed uuid, bytes initiator, bytes approver, SignedAssociationRecord sar
     );
 
     /// @notice Emitted when a previously active SignedAssociationRecord is revoked.
     ///
-    /// @param revoker The indexed address of the account that revoked the association.
-    /// @param uuid The indexed unique identifier for the association.
-    event AssociatedRevoked(address indexed revoker, bytes32 indexed uuid);
+    /// @param revoker The indexed unique identifier for the association.
+    /// @param uuid The ERC-7930 address of the revoking account.
+    event AssociatedRevoked(bytes32 indexed uuid, bytes revoker);
 
 
     /// @notice Method for storing a completed SignedAssociationRecord.
@@ -51,8 +55,8 @@ interface AssociatedAccounts {
     /// @dev The signed association record must meet the following validation criteria upon storage:
     ///     1. `sar.originatedAt` <= block.timestamp
     ///     2. `sar.revokedAt` == 0 || `sar.revokedAt` < block.timestamp
-    ///     3. `sar.initiatorSignature` passes ECDSA or EIP-1271 signature validation.
-    ///     4. `sar.approverSignature` passes ECDSA or EIP-1271 signature validation.
+    ///     3. `sar.initiatorSignature` passes signature validation using `sar.initiatorCurve` recovery.
+    ///     4. `sar.approverSignature` passes signature validation using `sar.approverCurve` recovery.
     ///
     ///     Upon successful validation, the method must emit the `AssociationCreated` event.
     ///
@@ -70,8 +74,10 @@ interface AssociatedAccounts {
 
     /// @notice Method for fetching uuid(s) of associations between two addresses.
     ///
-    /// @param addr1 CAIP10 address of the first account. 
-    /// @param addr2 CAIP10 address of the second account.
+    /// @dev Addresses must be sorted according to {EIP-XXXX: Address Sorting}
+    /// 
+    /// @param addr1 ERC-7930 address of the first account. 
+    /// @param addr2 ERC-7930 address of the second account.
     ///
     /// @return areAssociated `true` if the accounts have at least one valid and active association, else `false`.
     /// @return uuids Array of uuids for each association between the two accounts.
