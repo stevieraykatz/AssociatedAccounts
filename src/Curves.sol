3172342461a64091e8439ad3a72e11e2f7f0e547
katzman <steve.katzman@coinbase.com>
2025-10-30 13:13:50 -0700

    Add curves enumeration

---
+++ src/Curves.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/// @notice Enumeration described in Key Types table of eip-draft-aa-account-configuration
+/// https://github.com/chunter-cb/EIPs/blob/enshrined-aa-validation/EIPS/eip-draft-aa-account-configuration.md
+
+bytes1 constant K1 = 0x01;
+bytes1 constant R1 = 0x02;
+bytes1 constant WEBAUTHN = 0x03;
+bytes1 constant BLS = 0x04;
